331cbd614df080ccae1074b8f2dd3b15b667d0d2
Cameron P <cameron.l.penne@ntnu.no>
2024-07-24 14:18:52 -0500

    Add demo.py

---
+++ demo/demo.py
@@ -0,0 +1,27 @@
+
+#import sys
+#sys.path.append('../')
+import sys
+
+sys.path.insert(0, '/home/cameron/Projects/hypso-package')
+
+# Import Satellite Object
+from hypso import Hypso
+
+
+import os
+
+dir_path = '/home/cameron/Dokumenter/Data/erie'
+nc_file = os.path.join(dir_path, 'erie_2022-08-27_1605Z-l1a.nc')
+points_file = os.path.join(dir_path, 'erie_2022-08-27_1605Z-bin3.points')
+
+#dir_path = '/home/cameron/Dokumenter/Data/erie'
+#nc_file = os.path.join(dir_path, 'erie_2023-10-02_1559Z-l1a.nc')
+#points_file = os.path.join(dir_path, 'erie_2023-10-02_1559Z-bin3.points')
+
+
+satobj = Hypso(nc_file, points_path=points_file)
+
+
+
+
