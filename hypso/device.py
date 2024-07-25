a3d43295347d686139d08853982a4fd124489f8b
Cameron P <cameron.l.penne@ntnu.no>
2024-07-25 10:39:42 -0500

    Add Hypso2 class (planned)

--- hypso/device.py
+++ hypso/device.py
@@ -1231,6 +1231,20 @@ class Hypso1(Hypso):
         return toa_reflectance
 
 
+class Hypso2(Hypso):
+    def __init__(self, hypso_path: str, points_path: Union[str, None] = None) -> None:
+        
+        """
+        Initialization of (planned) HYPSO-2 Class.
+
+        :param hypso_path: Absolute path to "L1a.nc" file
+        :param points_path: Absolute path to the corresponding ".points" files generated with QGIS for manual geo
+            referencing. (Optional. Default=None)
+
+        """
+
+        super().__init__(hypso_path=hypso_path, points_path=points_path)
+
 def set_or_create_attr(var, attr_name, attr_value) -> None:
     """
     Set or create an attribute on ".nc" file.
