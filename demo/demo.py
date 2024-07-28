56a505b2012a7a9a9d14787c194c86ae522e4211
Cameron P <cameron.l.penne@ntnu.no>
2024-07-27 20:45:19 -0500

    Rename to display_geometry_information()

--- demo/demo.py
+++ demo/demo.py
@@ -25,29 +25,3 @@ points_file = os.path.join(dir_path, 'erie_2022-08-27_1605Z-bin3.points')
 satobj = Hypso1(hypso_path=nc_file, points_path=points_file, verbose=True)
 
 
-print(satobj.sat_zenith_angles)
-print(satobj.sat_zenith_angles.shape)
-#print(satobj.rad_coeff_file)
-
-
-#plt.imshow(satobj.l1b_cube[:,:,40])
-#plt.savefig('test.png')
-
-#satobj.create_l1b_nc_file()
-
-#print(type(satobj.rad_coeffs))
-
-
-#print(type(satobj.timing['timestamps_srv']))
-
-
-#for idx, stamp in enumerate(satobj.timing['timestamps_srv']):
-#    current_txt = '{:4d}'.format(idx) + ', ' + str(stamp) + '\n'
-#    print(current_txt)
-
-#for key in satobj.timing.keys():
-#    print(key)
-#    print(type(satobj.timing[key]))
-
-
-#satobj.get_geometry_information()
