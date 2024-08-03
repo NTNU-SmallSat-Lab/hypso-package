a901d2777cec9c07f92bf244d7a6071b639101b2
Cameron P <cameron.l.penne@ntnu.no>
2024-08-03 16:33:49 -0500

    Update satpy_demo.py

--- demo/satpy_demo.py
+++ demo/satpy_demo.py
@@ -1,9 +1,33 @@
 
 from satpy import Scene
 
+import numpy as np
 import xarray as xr
+import pandas as pd
 
 #scn = Scene()
 #scn['my_dataset'] = Dataset(my_data_array, **my_info)
 
-d = xr.Dataset(data_vars)
+
+
+# Create a NumPy array representing some data
+data = np.random.rand(3, 2)  # Example 3D array (e.g., time, lat, lon)
+
+# Define dimensions and coordinates
+dims = ('latitudes', 'longitudes')
+coords = {
+    'latitudes': [10.0, 20.0, 30.0],
+    'longitudes': [100.0, 110.0]
+}
+
+# Create an xarray Dataset
+ds = xr.Dataset(
+    {'example_data': (dims, data)},
+    coords=coords
+)
+
+scn = Scene()
+
+scn['example_data'] = ds
+
+print(scn['example_data'])
