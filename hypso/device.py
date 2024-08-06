c892f07475a424831b8fe034c90b3c528e9e9469
Cameron P <cameron.l.penne@ntnu.no>
2024-08-06 08:05:46 -0500

    Update TODO comment

--- hypso/device.py
+++ hypso/device.py
@@ -261,7 +261,7 @@ class Hypso:
         return None
         
 
-# TODO move cubes and chl to xarray
+# TODO move cubes, chl, land_mask, cloud_mask to xarray
 class Hypso1(Hypso):
 
     def __init__(self, hypso_path: Union[str, Path], points_path: Union[str, Path, None] = None, verbose=False) -> None:
