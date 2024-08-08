02e344b5656bcb78f6fc8998c08f5e8f8472a4b1
Cameron P <cameron.l.penne@ntnu.no>
2024-08-08 09:49:00 -0500

    Fix self.bbox

--- hypso/hypso.py
+++ hypso/hypso.py
@@ -97,7 +97,7 @@ class Hypso:
         self.along_track_mean_gsd = None
         self.across_track_mean_gsd = None
         self.resolution = None
-        self.bbox
+        self.bbox = None
 
        # Initialize calibration file paths:
         self.rad_coeff_file = None
