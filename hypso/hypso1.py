be6bdce862453b28e50eda5d5131a13589f7d7b6
Cameron P <cameron.l.penne@ntnu.no>
2024-08-08 07:54:04 -0500

    Update georeferencing routine

--- hypso/hypso1.py
+++ hypso/hypso1.py
@@ -429,6 +429,8 @@ class Hypso1(Hypso):
             self.longitudes = gr.longitudes[:, ::-1]
             
             self._run_georeferencing_orientation()
+            self._compute_gsd()
+            self._compute_resolution()
 
             self.latitudes_original = self.latitudes
             self.longitudes_original = self.longitudes
@@ -472,6 +474,86 @@ class Hypso1(Hypso):
 
         return None
     
+    def _compute_gsd(self) -> None:
+
+        frame_count = self.frame_count
+        image_height = self.image_height
+
+        latitudes = self.latitudes
+        longitudes = self.longitudes
+
+        bbox_geodetic = [np.min(latitudes), 
+                         np.max(latitudes), 
+                         np.min(longitudes), 
+                         np.max(longitudes)]
+
+        utm_crs_list = prj.database.query_utm_crs_info(datum_name="WGS 84",
+                                                        area_of_interest=prj.aoi.AreaOfInterest(
+                                                        west_lon_degree=bbox_geodetic[2],
+                                                        south_lat_degree=bbox_geodetic[0],
+                                                        east_lon_degree=bbox_geodetic[3],
+                                                        north_lat_degree=bbox_geodetic[1], )
+                                                    )
+        
+        if self.VERBOSE:
+            print(f'[INFO] Using UTM map: ' + utm_crs_list[0].name, 'EPSG:', utm_crs_list[0].code)
+
+        # crs_25832 = prj.CRS.from_epsg(25832) # UTM32N
+        # crs_32717 = prj.CRS.from_epsg(32717) # UTM17S
+        crs_4326 = prj.CRS.from_epsg(4326)  # Unprojected [(lat,lon), probably]
+        source_crs = crs_4326
+        destination_epsg = int(utm_crs_list[0].code)
+        destination_crs = prj.CRS.from_epsg(destination_epsg)
+        latlon_to_proj = prj.Transformer.from_crs(source_crs, destination_crs)
+
+
+        pixel_coords_map = np.zeros([frame_count, image_height, 2])
+
+        for i in range(frame_count):
+            for j in range(image_height):
+                pixel_coords_map[i, j, :] = latlon_to_proj.transform(latitudes[i, j], 
+                                                                     longitudes[i, j])
+
+        # time line x and y differences
+        a = np.diff(pixel_coords_map[:, image_height // 2, 0])
+        b = np.diff(pixel_coords_map[:, image_height // 2, 1])
+        along_track_gsd = np.sqrt(a * a + b * b)
+        along_track_mean_gsd = np.mean(along_track_gsd)
+
+        # detector line x and y differences
+        a = np.diff(pixel_coords_map[frame_count // 2, :, 0])
+        b = np.diff(pixel_coords_map[frame_count // 2, :, 1])
+        across_track_gsd = np.sqrt(a * a + b * b)
+        across_track_mean_gsd = np.mean(across_track_gsd)
+
+
+        self.along_track_gsd = along_track_gsd
+        self.across_track_gsd = across_track_gsd
+
+        self.along_track_mean_gsd = along_track_mean_gsd
+        self.across_track_mean_gsd = across_track_mean_gsd
+
+        return None
+
+    def _compute_resolution(self) -> None:
+
+        distances = [self.along_track_gsd, 
+                      self.across_track_gsd, 
+                      self.along_track_mean_gsd, 
+                      self.across_track_mean_gsd]
+
+        filtered_distances = [d for d in distances if d is not None]
+
+        try:
+            resolution = max(filtered_distances)
+        except ValueError:
+            resolution = 0
+
+        self.resolution = resolution
+
+        return None
+
+
     def _check_georeferencing_has_run(self) -> bool:
 
         return self.georeferencing_has_run
@@ -805,6 +887,7 @@ class Hypso1(Hypso):
 
     # Geometry computation functions
 
+    # TODO: rename
     def _run_geometry_computation(self, overwrite: bool = False) -> None:
 
         if self._check_geometry_computation_has_run() and not overwrite:
@@ -2630,7 +2713,7 @@ class Hypso1(Hypso):
 
         latitude_attrs = {
                          'file_type': None,
-                         'resolution': None,
+                         'resolution': self.resolution,
                          'standard_name': 'latitude',
                          'units': 'degrees_north',
                          'resolution': -999,
@@ -2642,7 +2725,7 @@ class Hypso1(Hypso):
 
         longitude_attrs = {
                           'file_type': None,
-                          'resolution': None,
+                          'resolution': self.resolution,
                           'standard_name': 'longitude',
                           'units': 'degrees_east',
                           'resolution': -999,
@@ -2676,7 +2759,7 @@ class Hypso1(Hypso):
 
         attrs = {
                 'file_type': None,
-                'resolution': -999,
+                'resolution': self.resolution,
                 'name': None,
                 'standard_name': cube.attrs['description'],
                 'coordinates': ['latitude', 'longitude'],
@@ -2712,7 +2795,7 @@ class Hypso1(Hypso):
 
         attrs = {
                 'file_type': None,
-                'resolution': -999,
+                'resolution': self.resolution,
                 'name': None,
                 'standard_name': cube.attrs['description'],
                 'coordinates': ['latitude', 'longitude'],
@@ -2749,7 +2832,7 @@ class Hypso1(Hypso):
 
         attrs = {
                 'file_type': None,
-                'resolution': -999,
+                'resolution': self.resolution,
                 'name': None,
                 'standard_name': cube.attrs['description'],
                 'coordinates': ['latitude', 'longitude'],
@@ -2779,81 +2862,3 @@ class Hypso1(Hypso):
 
 
 
-    def _compute_gsd(self) -> None:
-
-        frame_count = self.frame_count
-        image_height = self.image_height
-
-        latitudes = self.latitudes
-        longitudes = self.longitudes
-
-        bbox_geodetic = [np.min(latitudes), 
-                         np.max(latitudes), 
-                         np.min(longitudes), 
-                         np.max(longitudes)]
-
-        utm_crs_list = prj.database.query_utm_crs_info(datum_name="WGS 84",
-                                                        area_of_interest=prj.aoi.AreaOfInterest(
-                                                        west_lon_degree=bbox_geodetic[2],
-                                                        south_lat_degree=bbox_geodetic[0],
-                                                        east_lon_degree=bbox_geodetic[3],
-                                                        north_lat_degree=bbox_geodetic[1], )
-                                                    )
-        
-        if self.VERBOSE:
-            print(f'[INFO] Using UTM map: ' + utm_crs_list[0].name, 'EPSG:', utm_crs_list[0].code)
-
-        # crs_25832 = prj.CRS.from_epsg(25832) # UTM32N
-        # crs_32717 = prj.CRS.from_epsg(32717) # UTM17S
-        crs_4326 = prj.CRS.from_epsg(4326)  # Unprojected [(lat,lon), probably]
-        source_crs = crs_4326
-        destination_epsg = int(utm_crs_list[0].code)
-        destination_crs = prj.CRS.from_epsg(destination_epsg)
-        latlon_to_proj = prj.Transformer.from_crs(source_crs, destination_crs)
-
-
-        pixel_coords_map = np.zeros([frame_count, image_height, 2])
-
-        for i in range(frame_count):
-            for j in range(image_height):
-                pixel_coords_map[i, j, :] = latlon_to_proj.transform(latitudes[i, j], 
-                                                                     longitudes[i, j])
-
-        # time line x and y differences
-        a = np.diff(pixel_coords_map[:, image_height // 2, 0])
-        b = np.diff(pixel_coords_map[:, image_height // 2, 1])
-        along_track_gsd = np.sqrt(a * a + b * b)
-        along_track_mean_gsd = np.mean(along_track_gsd)
-
-        # detector line x and y differences
-        a = np.diff(pixel_coords_map[frame_count // 2, :, 0])
-        b = np.diff(pixel_coords_map[frame_count // 2, :, 1])
-        across_track_gsd = np.sqrt(a * a + b * b)
-        across_track_mean_gsd = np.mean(across_track_gsd)
-
-
-        self.along_track_gsd = along_track_gsd
-        self.across_track_gsd = across_track_gsd
-
-        self.along_track_mean_gsd = along_track_mean_gsd
-        self.across_track_mean_gsd = across_track_mean_gsd
-
-        return None
-
-    def _compute_resolution(self) -> None:
-
-        distances = [self.along_track_gsd, 
-                      self.across_track_gsd, 
-                      self.along_track_mean_gsd, 
-                      self.across_track_mean_gsd]
-
-        filtered_distances = [d for d in distances if d is not None]
-
-        try:
-            resolution = max(filtered_distances)
-        except ValueError:
-            resolution = 0
-
-        self.resolution = resolution
-
-        return None
