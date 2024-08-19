7a21c4021ab04e5784cf3656cba11c985e317cf1
Cameron P <cameron.l.penne@ntnu.no>
2024-08-19 13:43:05 -0500

    Add l1a, l1b, l2a, and masks class properties

--- hypso/hypso.py
+++ hypso/hypso.py
@@ -1,5 +1,8 @@
 from pathlib import Path
 from typing import Union
+import xarray as xr
+from hypso.DataArrayValidator import DataArrayValidator
+import numpy as np
 
 class Hypso:
 
@@ -38,9 +41,12 @@ class Hypso:
         self.l2a_nc_name = None
 
         # Initialize datacubes
-        self.l1a_cube = None
-        self.l1b_cube = None
-        self.l2a_cube = None
+        #self.l1a_cube = None
+        #self.l1b_cube = None
+        #self.l2a_cube = None
+        self._l1a_cube = None
+        self._l1b_cube = None
+        self._l2a_cube = None
 
         # Initialize top of atmpshere (TOA) reflectance
         self.toa_reflectance = None
@@ -119,13 +125,16 @@ class Hypso:
         self.srf = None
 
         # Initilize land mask dict
-        self.land_mask = None
+        #self.land_mask = None
+        self._land_mask = None
 
         # Initilize cloud mask dict
-        self.cloud_mask = None
+        #self.cloud_mask = None
+        self._cloud_mask = None
 
         # Intialize unified mask
-        self.unified_mask = None
+        #self.unified_mask = None
+        self._unified_mask = None
 
         # Initialize chlorophyll estimates dict
         self.chl = {}
@@ -151,9 +160,15 @@ class Hypso:
         self.latitudes_original = None
         self.longitudes_original = None
 
+        # Other
+        self.dim_names_3d = ["y", "x", "band"]
+        self.dim_names_2d = ["y", "x"]
+
         # DEBUG
         self.DEBUG = False
         self.VERBOSE = False
+
+        self._test_variable = None
 
     def _set_hypso_path(self, hypso_path=None) -> None:
 
@@ -174,4 +189,186 @@ class Hypso:
             self.points_path = None
 
         return None
+
+
+    def _update_dataarray_attrs(self, data: xr.DataArray, attrs: dict) -> xr.DataArray:
+
+        for key, value in attrs.items():
+            if key not in data.attrs:
+                data.attrs[key] = value
+
+        return data
+
+    def _format_l1a_dataarray(self, data: Union[np.ndarray, xr.DataArray]) -> xr.DataArray:
+
+        attributes = {'level': "L1a",
+                      'units': "a.u.",
+                      'description': "Raw sensor value"
+                     }
+
+        v = DataArrayValidator(dims_shape=self.spatial_dimensions, dims_names=self.dim_names_3d)
+
+        data = v.validate(data=data)
+        data = self._update_dataarray_attrs(data, attributes)
+
+        return data
+    
+
+
+    def _format_l1b_dataarray(self, data: Union[np.ndarray, xr.DataArray]) -> xr.DataArray:
+
+        attributes = {'level': "L1b",
+                      'units': r'$mW\cdot  (m^{-2}  \cdot sr^{-1} nm^{-1})$',
+                      'description': "Radiance (L)"
+                     }
+
+        v = DataArrayValidator(dims_shape=self.spatial_dimensions, dims_names=self.dim_names_3d)
+
+        data = v.validate(data=data)
+        data = self._update_dataarray_attrs(data, attributes)
+
+        return data
+
+
+
+
+    def _format_l2a_dataarray(self, data: Union[np.ndarray, xr.DataArray]) -> xr.DataArray:
+
+        attributes = {'level': "L2a",
+                      'units': "a.u.",
+                      'description': "Reflectance (Rrs)",
+                      'correction': None
+                     }
+
+        v = DataArrayValidator(dims_shape=self.spatial_dimensions, dims_names=self.dim_names_3d)
+
+        data = v.validate(data=data)
+        data = self._update_dataarray_attrs(data, attributes)
+
+        return data
+
+
+    def _format_land_mask_dataarray(self, data: Union[np.ndarray, xr.DataArray]) -> xr.DataArray:
+
+        attributes = {
+                      'description': "Land mask",
+                      'method': None
+                     }
+
+        v = DataArrayValidator(dims_shape=self.spatial_dimensions, dims_names=self.dim_names_2d, num_dims=2)
+
+        data = v.validate(data=data)
+        data = self._update_dataarray_attrs(data, attributes)
+
+        return data
+
+
+    def _format_cloud_mask_dataarray(self, data: Union[np.ndarray, xr.DataArray]) -> xr.DataArray:
+
+        attributes = {
+                      'description': "Cloud mask",
+                      'method': None
+                     }
+
+        v = DataArrayValidator(dims_shape=self.spatial_dimensions, dims_names=self.dim_names_2d, num_dims=2)
+
+        data = v.validate(data=data)
+        data = self._update_dataarray_attrs(data, attributes)
+
+        return data
+
+
+
+
+    def _format_unified_mask_dataarray(self, data: Union[np.ndarray, xr.DataArray]) -> xr.DataArray:
+
+        attributes = {
+                      'description': "Unified mask",
+                      'land_mask_method': None,
+                      'cloud_mask_method': None
+                     }
+
+        v = DataArrayValidator(dims_shape=self.spatial_dimensions, dims_names=self.dim_names_2d, num_dims=2)
+
+        data = v.validate(data=data)
+        data = self._update_dataarray_attrs(data, attributes)
+
+        return data
+
+
+
+    @property
+    def l1a_cube(self):
+        return self._l1a_cube   
+
+    @l1a_cube.setter
+    def l1a_cube(self, value):
+        self._l1a_cube = self._format_l1a_dataarray(value)
+
+
+    @property
+    def l1b_cube(self):
+        return self._l1b_cube   
+
+    @l1b_cube.setter
+    def l1b_cube(self, value):
+        self._l1b_cube = self._format_l1b_dataarray(value)
+
+    @property
+    def l2a_cube(self):
+        return self._l2a_cube   
+
+    @l2a_cube.setter
+    def l2a_cube(self, value):
+        self._l2a_cube = self._format_l2a_dataarray(value)
+
+
+    @property
+    def land_mask(self):
+        return self._land_mask 
+
+    @land_mask.setter
+    def land_mask(self, value):
+        self._land_mask = self._format_land_mask_dataarray(value)
         
+
+
+    @property
+    def cloud_mask(self):
+        return self._cloud_mask   
+
+    @cloud_mask.setter
+    def cloud_mask(self, value):
+        self._cloud_mask = self._format_cloud_mask_dataarray(value)
+        
+
+
+    @property
+    def unified_mask(self):
+
+        if self.land_mask is None and self.cloud_mask is None:
+            return None
+        
+        elif self.land_mask is None:
+
+            unified_mask = self.cloud_mask.to_numpy()
+            self._unified_mask = self._format_unified_mask_dataarray(unified_mask)
+            self._unified_mask.attrs['cloud_mask_method'] = self.cloud_mask.attrs['method']
+
+        elif self.cloud_mask is None:
+            
+            unified_mask = self.land_mask.to_numpy()
+            self._unified_mask = self._format_unified_mask_dataarray(unified_mask)
+            self._unified_mask.attrs['land_mask_method'] = self.land_mask.attrs['method']
+        
+        else:
+            unified_mask = self.land_mask.to_numpy() | self.cloud_mask.to_numpy()
+            self._unified_mask = self._format_unified_mask_dataarray(unified_mask)
+            self._unified_mask.attrs['land_mask_method'] = self.land_mask.attrs['method']
+            self._unified_mask.attrs['cloud_mask_method'] = self.cloud_mask.attrs['method']
+
+        return self._unified_mask  
+
+    @unified_mask.setter
+    def unified_mask(self, value):
+        self._unified_mask = self._format_unified_mask_dataarray(value)
