ee645b095ba64f6cfcac4091806dcbd15e853806
Cameron P <cameron.l.penne@ntnu.no>
2024-08-20 11:52:39 -0500

    Update acolite testing script with plots

--- testing/acolite_testing.py
+++ testing/acolite_testing.py
@@ -5,6 +5,9 @@ sys.path.insert(0, '/home/cameron/Projects/hypso-package')
 
 # Import Satellite Object
 from hypso import Hypso1
+from satpy.composites import GenericCompositor
+from satpy.writers import to_image
+import matplotlib.pyplot as plt
 
 
 dir_path = '/home/cameron/Dokumenter/Data/frohavet'
@@ -22,3 +25,35 @@ satobj.write_l2a_nc_file()
 
 
 print(satobj.l2a_cube)
+
+s = satobj.get_l2a_satpy_scene()
+
+# RGB Hypso image
+compositor = GenericCompositor("rgb")
+
+#R:630nm, G:550nm, B:480nm
+# Red, Green, Blue, Alpha
+red_wl = 630
+green_wl = 550
+blue_wl = 480
+
+red_idx = satobj.get_closest_wavelength_index(red_wl)
+green_idx = satobj.get_closest_wavelength_index(green_wl)
+blue_idx = satobj.get_closest_wavelength_index(blue_wl)
+
+red_band = 'band_' + str(red_idx)
+green_band = 'band_' + str(green_idx)
+blue_band = 'band_' + str(blue_idx)
+
+#composite = compositor([s[red_band][:,::3], s[green_band][:,::3], s[blue_band][:,::3], s['band_5'][:,::3]]) 
+composite = compositor([s[red_band][:,::3], s[green_band][:,::3], s[blue_band][:,::3]]) 
+
+rgb_img = to_image(composite) 
+rgb_img.stretch()
+rgb_img.gamma(2)
+
+rgb_xr_image = rgb_img
+
+rgb_img = rgb_img.pil_image()
+
+rgb_img.save('./' + satobj.capture_name + '_acolite_rgb.png')
