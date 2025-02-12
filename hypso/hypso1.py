e95896f725d4d5c399543e83afe0795e0c11e5c1
Cameron P <cameron.l.penne@ntnu.no>
2025-02-12 21:20:14 +0100

    Add spectral_analysis module

--- hypso/hypso1.py
+++ hypso/hypso1.py
@@ -16,7 +16,6 @@ import xarray as xr
 import hypso
 from hypso import Hypso
 
-from hypso.atmospheric import run_py6s, run_acolite, run_machi
 from hypso.calibration import read_coeffs_from_file, \
                               run_radiometric_calibration, \
                               run_destriping_correction, \
@@ -640,77 +639,8 @@ class Hypso1(Hypso):
 
 
 
-    # Public L1a methods
-
-    def get_l1a_spectrum(self, 
-                        latitude=None, 
-                        longitude=None,
-                        x: int = None,
-                        y: int = None
-                        ) -> xr.DataArray:
-
-        if self.l1a_cube is None:
-            return None
-        
-        if latitude is not None and longitude is not None:
-            idx = get_nearest_pixel(target_latitude=latitude, 
-                                    target_longitude=longitude,
-                                    latitudes=self.latitudes,
-                                    longitudes=self.longitudes)
-
-        elif x is not None and y is not None:
-            idx = (x,y)
-
-        else:
-            return None
-
-        spectrum = self.l1a_cube[idx[0], idx[1], :]
-
-        return spectrum
-
-    def plot_l1a_spectrum(self, 
-                         latitude=None, 
-                         longitude=None,
-                         x: int = None,
-                         y: int = None,
-                         save: bool = False
-                         ) -> None:
-        
-        if latitude is not None and longitude is not None:
-            idx = get_nearest_pixel(target_latitude=latitude, 
-                                    target_longitude=longitude,
-                                    latitudes=self.latitudes,
-                                    longitudes=self.longitudes)
-
-        elif x is not None and y is not None:
-            idx = (x,y)
-
-        else:
-            return None
-
-        spectrum = self.l1a_cube[idx[0], idx[1], :]
-        bands = range(0, len(spectrum))
-        units = spectrum.attrs["units"]
-
-        output_file = Path(self.parent_dir, self.capture_name + '_l1a_plot.png')
-
-        plt.figure(figsize=(10, 5))
-        plt.plot(bands, spectrum)
-        plt.ylabel(units)
-        plt.xlabel("Band number")
-        plt.title(f"L1a (lat, lon) --> (X, Y) : ({latitude}, {longitude}) --> ({idx[0]}, {idx[1]})")
-        plt.grid(True)
-
-        if save:
-            plt.imsave(output_file)
-        else:
-            plt.show()
-
-        return None
 
 
-
-    # Public L1b methods
 
     def generate_l1b_cube(self, **kwargs) -> None:
 
@@ -718,72 +648,8 @@ class Hypso1(Hypso):
 
         return None
 
-    def get_l1b_spectrum(self, 
-                        latitude=None, 
-                        longitude=None,
-                        x: int = None,
-                        y: int = None
-                        ) -> tuple[np.ndarray, str]:
 
-        if self.l1b_cube is None:
-            return None
-        
-        if latitude is not None and longitude is not None:
-            idx = get_nearest_pixel(target_latitude=latitude, 
-                                    target_longitude=longitude,
-                                    latitudes=self.latitudes,
-                                    longitudes=self.longitudes)
 
-        elif x is not None and y is not None:
-            idx = (x,y)
-            
-        else:
-            return None
-
-        spectrum = self.l1b_cube[idx[0], idx[1], :]
-
-        return spectrum
-
-    def plot_l1b_spectrum(self, 
-                        latitude=None, 
-                        longitude=None,
-                        x: int = None,
-                        y: int = None,
-                        save: bool = False
-                        ) -> None:
-        
-        if latitude is not None and longitude is not None:
-            idx = get_nearest_pixel(target_latitude=latitude, 
-                                    target_longitude=longitude,
-                                    latitudes=self.latitudes,
-                                    longitudes=self.longitudes)
-
-        elif x is not None and y is not None:
-            idx = (x,y)
-
-        else:
-            return None
-
-        spectrum = self.l1b_cube[idx[0], idx[1], :]
-        bands = self.wavelengths
-        units = spectrum.attrs["units"]
-
-        output_file = Path(self.parent_dir, self.capture_name + '_l1a_plot.png')
-
-        plt.figure(figsize=(10, 5))
-        plt.plot(bands, spectrum)
-        plt.ylabel(units)
-        plt.xlabel("Wavelength (nm)")
-        plt.title(f"L1b (lat, lon) --> (X, Y) : ({latitude}, {longitude}) --> ({idx[0]}, {idx[1]})")
-        plt.grid(True)
-
-        if save:
-            # TODO: TypeError: imsave() missing 1 required positional argument: 'arr'
-            plt.imsave(output_file)
-        else:
-            plt.show()
-
-        return None
 
     def write_l1b_nc_file(self, overwrite: bool = False) -> None:
 
@@ -826,75 +692,9 @@ class Hypso1(Hypso):
 
         return None
 
-    def get_l2a_spectrum(self,
-                        latitude=None, 
-                        longitude=None,
-                        x: int = None,
-                        y: int = None
-                        ) -> xr.DataArray:
 
 
-        if latitude is not None and longitude is not None:
-            idx = get_nearest_pixel(target_latitude=latitude, 
-                                    target_longitude=longitude,
-                                    latitudes=self.latitudes,
-                                    longitudes=self.longitudes)
 
-        elif x is not None and y is not None:
-            idx = (x,y)
-            
-        else:
-            return None
-
-        try:
-            spectrum = self.l2a_cube[idx[0], idx[1], :]
-        except KeyError:
-            return None
-
-        return spectrum
-
-    def plot_l2a_spectrum(self,
-                         latitude=None, 
-                         longitude=None,
-                         x: int = None,
-                         y: int = None,
-                         save: bool = False
-                         ) -> np.ndarray:
-        
-        if latitude is not None and longitude is not None:
-            idx = get_nearest_pixel(target_latitude=latitude, 
-                                    target_longitude=longitude,
-                                    latitudes=self.latitudes,
-                                    longitudes=self.longitudes)
-
-        elif x is not None and y is not None:
-            idx = (x,y)
-
-        else:
-            return None
-
-        try:
-            spectrum = self.l2a_cube[idx[0], idx[1], :]
-        except KeyError:
-            return None
-
-        bands = self.wavelengths
-        units = spectrum.attrs["units"]
-
-        output_file = Path(self.parent_dir, self.capture_name + '_l2a_plot.png')
-
-        plt.figure(figsize=(10, 5))
-        plt.plot(bands, spectrum)
-        plt.ylabel(units)
-        plt.ylim([0, 1])
-        plt.xlabel("Wavelength (nm)")
-        plt.title(f"L2a (lat, lon) --> (X, Y) : ({latitude}, {longitude}) --> ({idx[0]}, {idx[1]})")
-        plt.grid(True)
-
-        if save:
-            plt.imsave(output_file)
-        else:
-            plt.show()
 
     def write_l2a_nc_file(self, overwrite: bool = False) -> None:
 
