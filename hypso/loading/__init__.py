ca16a8220bfaf3f33bbdd01fc1bf18a442a9c5ea
Cameron P <cameron.l.penne@ntnu.no>
2025-02-13 09:58:05 +0100

    Add L1c NetCDF loader

--- hypso/loading/__init__.py
+++ hypso/loading/__init__.py
@@ -1,4 +1,4 @@
-from .loading import load_l1a_nc, load_l1a_nc_cube, load_l1a_nc_metadata
-from .loading import load_l1b_nc, load_l1b_nc_cube, load_l1b_nc_metadata
-#from .loading import load_l1c_nc, load_l1c_nc_cube, load_l1c_nc_metadata
-from .loading import load_l2a_nc, load_l2a_nc_cube, load_l2a_nc_metadata
+from .l1a_nc_loader import load_l1a_nc, load_l1a_nc_cube, load_l1a_nc_metadata
+from .l1b_nc_loader import load_l1b_nc, load_l1b_nc_cube, load_l1b_nc_metadata
+from .l1c_nc_loader import load_l1c_nc, load_l1c_nc_cube, load_l1c_nc_metadata
+from .l2a_nc_loader import load_l2a_nc, load_l2a_nc_cube, load_l2a_nc_metadata
