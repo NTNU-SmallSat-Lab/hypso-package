27794b5d73b0b275646187c2949b737cbcc4e89e
Cameron P <cameron.l.penne@ntnu.no>
2025-02-19 12:23:27 +0100

    Add PLSR code

---
+++ hypso/hypso/chlorophyll_estimation/plsr.py
@@ -0,0 +1,23 @@
+import os
+import numpy as np
+import xarray as xr
+from pathlib import Path
+
+from sklearn.cross_decomposition import PLSRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, r2_score
+
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_validate
+from sklearn.model_selection import KFold
+
+
+
+def generate_plsr_chl_estimates(satobj, model: Path):
+
+
+
+
+
+
+    return None
