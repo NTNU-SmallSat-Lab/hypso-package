816306f7787fd5b563314804bbbde77e8c715c60
ariaalinejad <ariaalinejad@gmail.com>
2025-10-09 17:19:00 +0200

    updates to srf implementaion for reflectance

--- hypso/hypso/calibration/srf.py
+++ hypso/hypso/calibration/srf.py
@@ -1,5 +1,4 @@
 import numpy as np
-import pickle
 
 
 # Updated function to process fwhm vector of length number of bands
@@ -17,10 +16,6 @@ def get_spectral_response_function(wavelengths, fwhm: np.array) -> None:
     srf = []
     for i, band in enumerate(wavelengths):
 
-
-        #if i == 119:
-        #    print("119")
-
         center_lambda_nm = band
         start_lambda_nm = np.round(center_lambda_nm - (3 * sigma_nm[i]), 4)
         soft_end_lambda_nm = np.round(center_lambda_nm + (3 * sigma_nm[i]), 4)
@@ -29,30 +24,33 @@ def get_spectral_response_function(wavelengths, fwhm: np.array) -> None:
         lower_wl = []
         upper_wl = []
 
+
+        # get the elements in the wavelength array that are 3Sigma above and below center_w
+        # if the center_w is closer to one end than 3Sigma then one of the arrays will be shorter 
+        # than the other. This needs to be corrected for so that the center is still kept
+        # when making the gaussian, therefore len_diff is found.
         for j, ele in enumerate(wavelengths):
             if start_lambda_nm < ele < center_lambda_nm:
                 lower_wl.append(ele)
             elif center_lambda_nm < ele < soft_end_lambda_nm:
                 upper_wl.append(ele)
 
-        #print(upper_wl)
-        #print(lower_wl)
-
         # Make symmetric
         if (len(wavelengths) - i) <= len(lower_wl):
-            # Close to highest wavelength, skip symmetry 
+            # Close to highest wavelength, find how many wavelengths missed because 
+            # 3 Sigma is out of wavelength bounds, skip symmetry 
             #print(i)
             #print("within upper limit")
             len_diff = len(lower_wl) - len(upper_wl)
-            pass
         elif i < len(upper_wl):
-            # Close to lowest wavelength, skip symmetry 
+            # Close to lowest wavelength, find how many wavelengths missed because 
+            # 3 Sigma is out of wavelength bounds, skip symmetry 
             #print(i)
             #print("within lower limit")
             len_diff = len(upper_wl) - len(lower_wl)
-            pass
         else:
             # Close to neither the highest nor lowest wavelength, enforce symmetry
+            # correcting for one beign one element longer than the other.
             while len(lower_wl) > len(upper_wl):
                 lower_wl.pop(0)
             while len(upper_wl) > len(lower_wl):
@@ -60,7 +58,8 @@ def get_spectral_response_function(wavelengths, fwhm: np.array) -> None:
             len_diff = 0
 
         srf_wl = lower_wl + srf_wl + upper_wl
-
+        # here a mask is made that tells us which wavelength go into the srf funciton
+        # of the wavelength in focus
         good_idx = [(True if ele in srf_wl else False) for ele in wavelengths]
 
         # Delta based on Hypso Sampling (Wavelengths)
@@ -68,6 +67,9 @@ def get_spectral_response_function(wavelengths, fwhm: np.array) -> None:
         if len(srf_wl) == 1:
             gx = [0]
         else:
+            # len_diff is added to make up for the missing elements because of clipping
+            # at the ends mentioned above. this replaces the clipped elements and makes sure
+            # gaussian has correct width
             gx = np.linspace(-3 * sigma_nm[i], 3 * sigma_nm[i], len(srf_wl) + len_diff)
         gaussian_srf = np.exp(
             -(gx / sigma_nm[i]) ** 2 / 2)  # Not divided by the sum, because we want peak to 1.0
@@ -76,7 +78,10 @@ def get_spectral_response_function(wavelengths, fwhm: np.array) -> None:
         srf_wl_single = wavelengths
         srf_single = np.zeros_like(srf_wl_single)
         
-        
+
+        # here the code checks if the gaussian function wants values that are lower or higher than the
+        # highest and lowest wavelength of hypso. If it does the function is clipped to only 
+        # be defined where hypso has vales 
         N = len(gaussian_srf)
         M = len(srf_single)
         half_N = N // 2
@@ -93,69 +98,10 @@ def get_spectral_response_function(wavelengths, fwhm: np.array) -> None:
 
         gaussian_srf_subset = gaussian_srf[start_idx:end_idx]
 
+        # just put the gaussian on to the right wavelength placement on the wavelength axis.
         srf_single[good_idx] = gaussian_srf_subset
 
-        srf.append([srf_wl_single, srf_single])
-
-
-    #with open('srf_new.pkl', 'wb') as file:
-    #    pickle.dump(srf, file)
-
-    return srf
-
-
-# Updated function to process fwhm vector of length number of bands
-def get_spectral_response_function_thuillier_2002(wavelengths, fwhm: np.array) -> None:
-    """
-    Get Spectral Response Functions (SRF) from HYPSO for each of the 120 bands. Theoretical FWHM of 3.33nm is
-    used to estimate Sigma for an assumed gaussian distribution of each SRF per band.
-
-    :return: None.
-    """
-
-    fwhm_nm = fwhm
-    sigma_nm = fwhm_nm / (2 * np.sqrt(2 * np.log(2)))
-
-    srf = []
-    for i, band in enumerate(wavelengths):
-        center_lambda_nm = band
-        start_lambda_nm = np.round(center_lambda_nm - (3 * sigma_nm[i]), 4)
-        soft_end_lambda_nm = np.round(center_lambda_nm + (3 * sigma_nm[i]), 4)
-
-        srf_wl = [center_lambda_nm]
-        lower_wl = []
-        upper_wl = []
-        for ele in wavelengths:
-            if start_lambda_nm < ele < center_lambda_nm:
-                lower_wl.append(ele)
-            elif center_lambda_nm < ele < soft_end_lambda_nm:
-                upper_wl.append(ele)
-
-        # Make symmetric
-        while len(lower_wl) > len(upper_wl):
-            lower_wl.pop(0)
-        while len(upper_wl) > len(lower_wl):
-            upper_wl.pop(-1)
-
-        srf_wl = lower_wl + srf_wl + upper_wl
-
-        good_idx = [(True if ele in srf_wl else False) for ele in wavelengths]
-
-        # Delta based on Hypso Sampling (Wavelengths)
-        gx = None
-        if len(srf_wl) == 1:
-            gx = [0]
-        else:
-            gx = np.linspace(-3 * sigma_nm[i], 3 * sigma_nm[i], len(srf_wl))
-        gaussian_srf = np.exp(
-            -(gx / sigma_nm[i]) ** 2 / 2)  # Not divided by the sum, because we want peak to 1.0
-
-        # Get final wavelength and SRF
-        srf_wl_single = wavelengths
-        srf_single = np.zeros_like(srf_wl_single)
-        srf_single[good_idx] = gaussian_srf
-
-        srf.append([srf_wl_single, srf_single])
+        srf.append(gaussian_srf)
 
     return srf
 
--- hypso/hypso/reflectance/toa_reflectance.py
+++ hypso/hypso/reflectance/toa_reflectance.py
@@ -3,29 +3,17 @@ from importlib.resources import files
 from dateutil import parser
 
 import numpy as np
-import pandas as pd
 import xarray as xr
+from scipy.interpolate import CubicSpline
 
 
 
 def compute_toa_reflectance(srf,
+                            wavelengths,
                             toa_radiance: np.ndarray,
                             iso_time,
                             solar_zenith_angles,
                             ) -> xr.DataArray:
-
-    if True:
-        compute_toa_reflectance_thuillier_2002(srf=srf, 
-                                          toa_radiance=toa_radiance, 
-                                          iso_time=iso_time, 
-                                          solar_zenith_angles=solar_zenith_angles)
-
-    # Get Local variables
-    #srf = self.srf
-    #toa_radiance = self.l1b_cube.to_numpy()
-
-    #iso_time = self.iso_time
-    #solar_zenith_angles = self.solar_zenith_angles
 
     scene_date = parser.isoparse(iso_time)
     julian_day = scene_date.timetuple().tm_yday
@@ -34,9 +22,6 @@ def compute_toa_reflectance(srf,
     # Load the NetCDF file
     solar_data_path = str(files('hypso.reflectance').joinpath("hybrid_reference_spectrum_p1nm_resolution_c2022-11-30_with_unc.nc"))
     ds = xr.open_dataset(solar_data_path)
-    #print(ds)
-    #print(ds.data_vars)
-    #print(ds.attrs)
 
     solar_x = ds["Vacuum Wavelength"].values
     solar_y = ds["SSI"].values * 1000 # convert to milliwatts
@@ -57,133 +42,62 @@ def compute_toa_reflectance(srf,
     # Interpolate for Y with original solar data
     new_solar_y = np.interp(new_solar_x, solar_x[:], solar_y[:])
 
-    import pickle
-    with open('ssi.pkl', 'wb') as file:
-        pickle.dump(new_solar_y, file)
-
-    with open('ssi_wl.pkl', 'wb') as file:
-        pickle.dump(new_solar_x, file)
-
-
     ssi_values = new_solar_y
-    # Replace solar Dataframe
-    #solar_df = pd.DataFrame(np.column_stack((new_solar_x, new_solar_y)), columns=solar_df.columns)
 
     # Estimation of TOA Reflectance
     band_number = 0
     toa_reflectance = np.empty_like(toa_radiance)
 
     ESUN_hypso = []
+    
+    for i in range(len(srf)):
+        single_wl = wavelengths
+        single_srf = np.array(srf[i])
 
-    for single_wl, single_srf in srf:
+        # check if the srf goes outside the wavelength range of hypso, if so
+        # extend the wavelength range with a cubic spline
+        if (len(single_srf) // 2 > i): 
+            # check if srf wants to fetch values lower than the lowest wavelength of hypso
+            x_orig = np.arange(0, len(wavelengths))
+            interpolator = CubicSpline(x_orig, wavelengths, extrapolate=True)        
+            x_new = np.arange(-np.abs(i - len(single_srf) // 2), len(wavelengths))
+            single_wl = interpolator(x_new)
+        elif (i + (len(single_srf)//2) >= len(wavelengths)):
+            # check if srf wants to fetch values higher than the highest wavelength of hypso
+            x_orig = np.arange(0, len(wavelengths))
+            interpolator = CubicSpline(x_orig, wavelengths, extrapolate=True)        
+            x_new = np.arange(0, i + (len(single_srf)//2) + 1)
+            single_wl = interpolator(x_new)
+        # put the srf funciton on the wavelength axis, assuming srf function is symmetric
+        k = 0 
+        single_srf_on_wl_axis = np.zeros_like(single_wl)
+        for j in range(-(len(single_srf)//2), len(single_srf)//2+1):
+            center_idx = np.argwhere(single_wl == wavelengths[i])
+            single_srf_on_wl_axis[center_idx + j] = single_srf[k]
+            k += 1
+        
+        # for single_wl, single_srf in (wavelengths, srf):
         # Resample HYPSO SRF to new solar wavelength
-        resamp_srf = np.interp(new_solar_x, single_wl, single_srf, left=0, right=0)
-        #resamp_srf = np.interp(new_solar_x, single_wl, single_srf)
+        resamp_srf = np.interp(new_solar_x, single_wl, single_srf_on_wl_axis, left=0, right=0)
         resamp_srf_sum = np.sum(resamp_srf)
         weights_srf = resamp_srf / resamp_srf_sum
-
-        #import matplotlib.pyplot as plt
-        #plt.plot(range(0,len(resamp_srf)), resamp_srf)
-        #plt.savefig('resamp_srf.png')
-        #plt.close()
-
-        #plt.plot(range(0,len(weights_srf)), weights_srf)
-        #plt.savefig('weights_srf.png')
-        #plt.close()
 
         #ESUN = np.sum(solar_df['mW/m2/nm'].values * weights_srf)  # units matche HYPSO from device.py
         ESUN = np.sum(ssi_values * weights_srf)  # units matche HYPSO from device.py
         ESUN_hypso.append(ESUN)
-        # Earth-Sun distance (from day of year) using julian date
+        # Earth-Sun distance scaler (from day of year) using julian date
+        # (R/R_0) earth-sun distance divided by average earth-sun distance
         # http://physics.stackexchange.com/questions/177949/earth-sun-distance-on-a-given-day-of-the-year
-        distance_sun = 1 - 0.01672 * np.cos(0.9856 * (
-                julian_day - 4))
+        sun_distance_scaler = 1 - 0.01672 * np.cos(0.9856 * (
+                julian_day - 4)) # 4 is when earth reaches perihelion, day 4 for 2025 
 
         # Get toa_reflectance
+        # equation for "Normalized reflectances" found here:
+        # https://oceanopticsbook.info/view/atmospheric-correction/normalized-reflectances 
         solar_angle_correction = np.cos(np.radians(solar_zenith_angles))
-        multiplier = (ESUN * solar_angle_correction) / (np.pi * distance_sun ** 2)
+        multiplier = (ESUN * solar_angle_correction) / (np.pi * sun_distance_scaler ** 2)
         toa_reflectance[:, :, band_number] = toa_radiance[:, :, band_number] / multiplier
 
         band_number = band_number + 1
-
-    #toa_reflectance = -toa_reflectance
-
-    #with open('esun_hypso.pkl', 'wb') as file:
-    #    pickle.dump(np.array(ESUN_hypso), file)
-
-    return toa_reflectance
-
-
-
-
-
-
-
-def compute_toa_reflectance_thuillier_2002(srf,
-                            toa_radiance: np.ndarray,
-                            iso_time,
-                            solar_zenith_angles,
-                            ) -> xr.DataArray:
-
-    
-    # Get Local variables
-    #srf = self.srf
-    #toa_radiance = self.l1b_cube.to_numpy()
-
-    #iso_time = self.iso_time
-    #solar_zenith_angles = self.solar_zenith_angles
-
-    scene_date = parser.isoparse(iso_time)
-    julian_day = scene_date.timetuple().tm_yday
-
-
-    # Read Solar Data
-    solar_data_path = str(files('hypso.reflectance').joinpath("Solar_irradiance_Thuillier_2002.csv"))
-    solar_df = pd.read_csv(solar_data_path)
-
-    # Create new solar X with a new delta
-    solar_array = np.array(solar_df)
-    current_num = solar_array[0, 0]
-    delta = 0.01
-    new_solar_x = [solar_array[0, 0]]
-    while current_num <= solar_array[-1, 0]:
-        current_num = current_num + delta
-        new_solar_x.append(current_num)
-
-    # Interpolate for Y with original solar data
-    new_solar_y = np.interp(new_solar_x, solar_array[:, 0], solar_array[:, 1])
-
-    # Replace solar Dataframe
-    solar_df = pd.DataFrame(np.column_stack((new_solar_x, new_solar_y)), columns=solar_df.columns)
-
-    # Estimation of TOA Reflectance
-    band_number = 0
-    toa_reflectance = np.empty_like(toa_radiance)
-
-    ESUN_hypso = []
-
-    for single_wl, single_srf in srf:
-        # Resample HYPSO SRF to new solar wavelength
-        resamp_srf = np.interp(new_solar_x, single_wl, single_srf)
-        weights_srf = resamp_srf / np.sum(resamp_srf)
-        ESUN = np.sum(solar_df['mW/m2/nm'].values * weights_srf)  # units matche HYPSO from device.py
-        ESUN_hypso.append(ESUN)
-        # Earth-Sun distance (from day of year) using julian date
-        # http://physics.stackexchange.com/questions/177949/earth-sun-distance-on-a-given-day-of-the-year
-        distance_sun = 1 - 0.01672 * np.cos(0.9856 * (
-                julian_day - 4))
-
-        # Get toa_reflectance
-        solar_angle_correction = np.cos(np.radians(solar_zenith_angles))
-        multiplier = (ESUN * solar_angle_correction) / (np.pi * distance_sun ** 2)
-        toa_reflectance[:, :, band_number] = toa_radiance[:, :, band_number] / multiplier
-
-        band_number = band_number + 1
-
-    #toa_reflectance = -toa_reflectance
-
-    #import pickle
-    #with open('esun_hypso_thuillier.pkl', 'wb') as file:
-    #    pickle.dump(np.array(ESUN_hypso), file)
 
     return toa_reflectance
