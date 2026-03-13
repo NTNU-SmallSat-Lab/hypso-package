2546ff96a576d4a13cca4d6abda4640938ac2cb4
Aria Alinejad <ariaalinejad@gmail.com>
2026-03-13 16:16:30 +0100

    minor changes

--- hypso/hypso/hypso1.py
+++ hypso/hypso/hypso1.py
@@ -1,19 +1,22 @@
-from pathlib import Path
-from typing import Union
-import numpy as np
 import copy
 
 # Development use only:
 import sys
-sys.path.insert(0, '/home/ariaa/smallSatLab/hypso-package-new/hypso1_calibration/')
-# sys.path.append('../../hypso-package-new/hypso1_calibration/')
+from pathlib import Path
+from typing import Union
+
+import numpy as np
+
+sys.path.insert(0, "/home/ariaa/smallSatLab/hypso-package/hypso1_calibration/")
+# sys.path.append('../../hypso-package/hypso1_calibration/')
+
+from hypso1_calibration import get_hypso1_calibration_files
 
 from .HypsoBase import HypsoBase
-from hypso1_calibration import get_hypso1_calibration_files
-class Hypso1(HypsoBase):
 
+
+class Hypso1(HypsoBase):
     def __init__(self, path: Union[str, Path], verbose=False) -> None:
-        
         """
         Initialization of HYPSO-1 Class.
 
@@ -26,20 +29,18 @@ class Hypso1(HypsoBase):
         super().__init__(path=path)
 
         # General -----------------------------------------------------
-        self.platform = 'hypso1'
-        self.sensor = 'hypso1_hsi'
+        self.platform = "hypso1"
+        self.sensor = "hypso1_hsi"
         self.VERBOSE = verbose
 
-        self.srf_wl = np.array([455 ,505 ,555 ,605 ,655 ,705, 755 ])
+        self.srf_wl = np.array([455, 505, 555, 605, 655, 705, 755])
         self.srf_fwhm = np.array([9.6, 6.6, 8.2, 5.8, 5.8, 4.1, 4.0])
 
         self._load_capture_file(path=path)
 
         return None
 
-
-
-    def _set_calibration_coeff_files(self, **kwargs) -> None:            
+    def _set_calibration_coeff_files(self, **kwargs) -> None:
         """
         Set the absolute path for the calibration coefficients included in the package. This includes radiometric,
         smile and destriping correction.
@@ -49,27 +50,19 @@ class Hypso1(HypsoBase):
 
         capture_type = self.capture_type
 
-        coeff_type = kwargs.get('coeff_type', 'original')  # Default to 'original' if not provided
-        print(f"[INFO] - Setting calibration coefficient files with coeff_type: {coeff_type}")
-        calibration_files = get_hypso1_calibration_files(capture_type, coeff_type=coeff_type)  # 'moved', 'adjusted', or 'original.'
+        coeff_type = kwargs.get(
+            "coeff_type", "original"
+        )  # Default to 'original' if not provided
+        print(
+            f"[INFO] - Setting calibration coefficient files with coeff_type: {coeff_type}"
+        )
+        calibration_files = get_hypso1_calibration_files(
+            capture_type, coeff_type=coeff_type
+        )  # 'moved', 'adjusted', or 'original.'
 
-        self.rad_coeff_file = calibration_files['radiometric']
-        self.smile_coeff_file = calibration_files['smile']
-        self.destriping_coeff_file = calibration_files['destriping']
-        self.spectral_coeff_file = calibration_files['spectral']
+        self.rad_coeff_file = calibration_files["radiometric"]
+        self.smile_coeff_file = calibration_files["smile"]
+        self.destriping_coeff_file = calibration_files["destriping"]
+        self.spectral_coeff_file = calibration_files["spectral"]
 
         return None
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- hypso/hypso/hypso2.py
+++ hypso/hypso/hypso2.py
@@ -1,19 +1,22 @@
-from pathlib import Path
-from typing import Union
-import numpy as np
 import copy
 
 # Development use only:
 import sys
-sys.path.insert(0, '/home/ariaa/smallSatLab/hypso-package-new/hypso2_calibration/')
-# sys.path.append('../../hypso-package-new/hypso2_calibration/')
+from pathlib import Path
+from typing import Union
+
+import numpy as np
+
+sys.path.insert(0, "/home/ariaa/smallSatLab/hypso-package/hypso2_calibration/")
+# sys.path.append('../../hypso-package/hypso2_calibration/')
+
+from hypso2_calibration import get_hypso2_calibration_files
 
 from .HypsoBase import HypsoBase
-from hypso2_calibration import get_hypso2_calibration_files
-class Hypso2(HypsoBase):
 
+
+class Hypso2(HypsoBase):
     def __init__(self, path: Union[str, Path], verbose=False) -> None:
-        
         """
         Initialization of HYPSO-2 Class.
 
@@ -26,51 +29,65 @@ class Hypso2(HypsoBase):
         super().__init__(path=path)
 
         # General -----------------------------------------------------
-        self.platform = 'hypso2'
-        self.sensor = 'hypso2_hsi'
+        self.platform = "hypso2"
+        self.sensor = "hypso2_hsi"
         self.VERBOSE = verbose
 
-        self.srf_wl =   [435.84 ,546.07 ,696.54 ,706.72 ,738.4 ,751.46 ,763.51 ,772.38 ,811.53 ,826.45 ,842.46 ,871.68 ,912 ]
-        self.srf_fwhm = [5.46   ,3.34   ,3.29   ,3.32   ,3.42  ,3.54   ,3.58   ,3.59   ,4.16   ,4.06   ,4.66   ,4.47   ,5.06]
-
+        self.srf_wl = [
+            435.84,
+            546.07,
+            696.54,
+            706.72,
+            738.4,
+            751.46,
+            763.51,
+            772.38,
+            811.53,
+            826.45,
+            842.46,
+            871.68,
+            912,
+        ]
+        self.srf_fwhm = [
+            5.46,
+            3.34,
+            3.29,
+            3.32,
+            3.42,
+            3.54,
+            3.58,
+            3.59,
+            4.16,
+            4.06,
+            4.66,
+            4.47,
+            5.06,
+        ]
 
         self._load_capture_file(path=path)
 
         return None
 
-
-
-    def _set_calibration_coeff_files(self, **kwargs) -> None:     
+    def _set_calibration_coeff_files(self, **kwargs) -> None:
         """
         Set the absolute path for the calibration coefficients included in the package. This includes radiometric,
         smile and destriping correction.
 
         :return: None.
-        """       
-        coeff_type = kwargs.get('coeff_type', 'original')  # Default to 'original' if not provided
-        print(f"[INFO] - Setting calibration coefficient files with coeff_type: {coeff_type}")
-        calibration_files = get_hypso2_calibration_files(coeff_type=coeff_type)  # 'moved', 'adjusted', or 'original.'
+        """
+        coeff_type = kwargs.get(
+            "coeff_type", "original"
+        )  # Default to 'original' if not provided
+        print(
+            f"[INFO] - Setting calibration coefficient files with coeff_type: {coeff_type}"
+        )
+        calibration_files = get_hypso2_calibration_files(
+            coeff_type=coeff_type
+        )  # 'moved', 'adjusted', or 'original.'
 
-        self.rad_coeff_file = calibration_files['radiometric']
-        self.smile_coeff_file = calibration_files['smile']
-        self.destriping_coeff_file = calibration_files['destriping']
-        self.spectral_coeff_file = calibration_files['spectral']
+        self.rad_coeff_file = calibration_files["radiometric"]
+        self.smile_coeff_file = calibration_files["smile"]
+        self.destriping_coeff_file = calibration_files["destriping"]
+        self.spectral_coeff_file = calibration_files["spectral"]
 
         return None
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
