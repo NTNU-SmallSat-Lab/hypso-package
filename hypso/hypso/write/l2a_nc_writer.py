e084091032d90f98d347ee529d87d80ecdf6bdc3
Cameron Penne <cameron.l.penne@ntnu.no>
2026-06-02 15:52:38 +0200

    Update l2a_nc_writer.py

--- hypso/hypso/write/l2a_nc_writer.py
+++ hypso/hypso/write/l2a_nc_writer.py
@@ -24,7 +24,7 @@ def write_l2a_nc_file(satobj, correction: str = None, overwrite: bool = False, *
 
     for correction in l2a_nc_file_list:
 
-        l2a_nc_file = Path(parent_dir, satobj.l2a_name + "-" + str(correction) + ".nc")
+        l2a_nc_file = Path(parent_dir, satobj.l2a_name() + "-" + str(correction) + ".nc")
 
         print(l2a_nc_file)
 
